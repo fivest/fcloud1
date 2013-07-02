c83179021a8b2ee708af6f47b33b2b84f4c2a68c
fyxruben <fyxruben@gmail.com>
2013-07-01 22:20:16 -0700

    action相关

--- src/main/java/com/fcloud/modules/demo/mapper/DemoMainMapper.java
+++ src/main/java/com/fcloud/modules/demo/mapper/DemoMainMapper.java
@@ -1,20 +1,14 @@
 package com.fcloud.modules.demo.mapper;
 
 import com.fcloud.core.mapper.CrudMapper;
-import com.fcloud.core.mapper.Mapper;
 import com.fcloud.core.page.Pagination;
 import com.fcloud.modules.demo.model.DemoMain;
 
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: ruben
- * Date: 13-6-16
- * Time: 下午8:37
- * To change this template use File | Settings | File Templates.
+ * Demo Mapper
  */
-@Mapper
 public interface DemoMainMapper extends CrudMapper<DemoMain> {
 
     public List<DemoMain> findAll();
---
+++ src/main/webapp/WEB-INF/views/demo/main/edit.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Demo Main 编辑</title>
+</head>
+<body>
+编辑界面
+
+</body>
+</html>
